1515a6599af7d9811c42524ca3d758ab759e4a5e
Maxime Frelin <frelinmaxime@gmail.com>
2019-09-07 16:41:00 +0200

    Changement de la configuration du projet
    - Exclusion des fichiers compilés dans le gitignore
    - Ajout de configuration supplémentaire dans le compilateur

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import Authentification from "./modules/authentification/Authentification"
 const http = require('http');
 
 // Create an instance of the http server to handle HTTP requests
@@ -12,3 +13,5 @@ let app = http.createServer((req, res) => {
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');
 console.log('Node server running on port 3000');
+
+new Authentification();
