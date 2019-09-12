8f87d90b0cc7bdc9566699f083875fc644a20a16
Maxime Frelin <frelinmaxime@gmail.com>
2019-09-12 16:57:06 +0200

    Ajout de la configuration

---
+++ src/config/configuration.js
@@ -0,0 +1,13 @@
+export default {
+    db: {
+        connectionString: "postgres://postgres:localpgadmin@localhost:5432/authentification"
+    },
+    cache: {
+        enable: true,
+        duration: 36000, //milliseconds
+        checkDelay: 5000 //milliseconds
+    },
+    authentification: {
+        useCache: true
+    }
+}
