5ddc77e34747a902c22218c3f6ad336eb5ca31ac
Arthur <ligerarthur@gmail.com>
2019-09-12 15:13:58 +0200

    module DAO

---
+++ src/modules/DAO.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql');
+
+class DAO {
+    constructor(conf) {
+      this.ip = conf.ip;
+      this.id = conf.id;
+      this.pass = conf.pass;
+      this.port = conf.port;
+      this.db = conf.db;
+    }
+}
+
+DAO.prototype.connect = function(){
+ 
+    console.log('Get connection ...');
+ 
+    this.connexion = mysql.createConnection({
+        database: this.db,
+        host: this.ip,
+        user: this.id,
+        password: this.pass
+    });
+ 
+    this.connexion.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+    });
+
+    return this.connexion;
+
+}
+
+
+module.exports = DAO;
