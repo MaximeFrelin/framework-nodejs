0ac0d34111ea6b44bc28c5536582371b8e401ac8
Sébastien <medionsoul@hotmail.fr>
2019-10-14 16:05:24 +0200

    Ajout templating de variables.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import Authentification from "./modules/authentification/Authentification"
 const http = require('http');
+import { render } from "./templating.js"
 
 // Create an instance of the http server to handle HTTP requests
 let app = http.createServer((req, res) => {
     // Set a response type of plain text for the response
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-
+    res.write(render("Bonjour {{ nom }} {{ prenom}} ! Vous avez {{ age }} ans et êtes {{ profession }}",{nom : "Ducasse", prenom : "Sébastien", age : 22, profession : "apprenti"}));
     // Send back a response and end the connection
-    res.end('Hello World!\n');
-    new Authentification();
-    new Authentification().testDecorator();
+    res.end('');
 });
 
 // Start the server on port 3000
