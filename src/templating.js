1a01de875f275225ea9303fcdd8878666483f8d3
Sébastien <medionsoul@hotmail.fr>
2019-10-17 14:03:43 +0200

    Ajout page formulaire + page de résultat template html

--- src/templating.js
+++ src/templating.js
@@ -1,12 +1,12 @@
 export function render(page, args){
-    let code = page.match(/[{]{2}[\w\s]*[}]{2}/g);
+    let code = page.match(/[{]{2}[\w\s]*[}]{2}/g); // cherche {{ mot }}
     code.forEach(element => {
     //    ar.push(element.substring(2,element.length-2).trim());
     let key = element.substring(2, element.length-2).trim();
     if(element.substring(0,2) == "{{" && element.substring(element.length-2, element.length) == "}}")
     {
         var regex = new RegExp("[{]{2}[\\s\\b" +  key +"]*[}]{2}")
-        page = page.replace(regex,args[key]);
+        page = page.replace(regex,args[key]); //remplace {{ mot }} par la valeur de la variable mot
     }
     });
     console.log(page);
